fa2ce8afd114bd3c0cd8e647618d05d7c5470bdf
Iresh Eranga <117840288+Iresheranga@users.noreply.github.com>
2023-02-16 20:21:00 +0530

    Create 01.cpp

---
+++ Simple Exercises/01.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+	std::cout<<"My first name is Iresh."<<std::endl;
+	std::cout<<"My home town is Kurunegala."<<std::endl;
+	std::cout<<"My school is Maliyadewa Boys College."<<std::endl;
+	
+	return 0;
+}
