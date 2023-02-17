30a372885e2419b7572d8207492c6b9875fa0a00
Iresh Eranga <117840288+Iresheranga@users.noreply.github.com>
2023-02-17 08:41:24 +0530

    Create 02.cpp
    Write a C++ program to input the length and the width of a rectangle and calculate and print the perimeter.

---
+++ Simple Exercises/02.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()		//main function begin
+{
+	float length, width, perimeter;		//variable declaration
+	
+	cout<<"Enter Length of rectangle :";	//get user inputs
+	cin>>length;
+	
+	cout<<"Enter width of rectangle :";
+	cin>>width;
+	
+	perimeter = 2 * (length + width);		//formular to calculate the perimeter of rectangle
+	
+	cout<<"Perimeter of rectangle = "<<perimeter<<endl;		//display the perimeter of rectangle
+	
+	return 0;
+}		//end of the main function
